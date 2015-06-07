a941a878a0d73f53465ae6924876adb26f5bbdb3
Tigran Terian <tigran.terian@gmail.com>
2015-06-07 19:09:56 +0400

    tweaked post migration

--- db/migrate/20150607145556_create_posts.rb
+++ db/migrate/20150607145556_create_posts.rb
@@ -1,12 +1,14 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :image
-      t.text :content
-      t.string :tags
-      t.references :user, index: true, foreign_key: true
+      t.string :image, null: false, default: ''
+      t.text   :content, null: false, default: ''
+      t.string :tags, null: false, default: [], array: true
+      t.integer :user_id, null: false
 
       t.timestamps null: false
     end
+
+    add_index :posts, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607092908) do
+ActiveRecord::Schema.define(version: 20150607145556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "image",      default: "", null: false
+    t.text     "content",    default: "", null: false
+    t.string   "tags",       default: [], null: false, array: true
+    t.integer  "user_id",                 null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name",              default: "", null: false
